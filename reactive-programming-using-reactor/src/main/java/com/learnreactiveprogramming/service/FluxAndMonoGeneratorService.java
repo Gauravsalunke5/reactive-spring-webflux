a47f90bd908ba8e0784ea78f578cfbe87838c653
Gaurav Salunke <gauravsalunke15@gmail.com>
2022-11-13 18:49:54 +0530

    flux and mono,

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -1,4 +1,49 @@
 package com.learnreactiveprogramming.service;
 
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
 public class FluxAndMonoGeneratorService {
+    public Flux<String> namesFlux() {
+        return Flux.fromIterable(List.of("Gaurav", "G2", "g3")).log(); // db or remove service
+    }
+
+    public Mono<String> nameMono() {
+        return Mono.just("GAURAV");
+    }
+
+    public Flux<String> namesFlux_map() {
+        return Flux.fromIterable(List.of("Gaurav", "G2", "g3"))// db or remove service
+                .map(String::toUpperCase)
+                // .map(s->s.toUpperCase())
+                .log();
+    }
+
+    public Flux<String> namesFlux_immutability() {
+        var namesFlux = Flux.fromIterable(List.of("Gaurav", "G2", "g3"));// db or remove service
+        namesFlux.map(String::toUpperCase);
+        return namesFlux;
+    }
+
+    public Flux<String> namesFlux_map(int stringLength) {
+        // filter the string whose length greater than 3
+        return Flux.fromIterable(List.of("Gaurav", "G2", "g3"))// db or remove service
+                .map(String::toUpperCase)
+                .filter(s -> s.length() > stringLength)
+                .map(s-> s.length() +"-"+s) //6-GAURAV
+                .log();
+    }
+
+    public static void main(String[] args) {
+        FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+        fluxAndMonoGeneratorService.namesFlux().subscribe(name -> {
+            System.out.println("Flux Name is " + name);
+        });
+
+        fluxAndMonoGeneratorService.nameMono().subscribe(name -> {
+            System.out.println("Mono Name is " + name);
+        });
+    }
 }
--- reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
+++ reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
@@ -1,7 +1,63 @@
 package com.learnreactiveprogramming.service;
 
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FluxAndMonoGeneratorServiceTest {
 
+    FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+
+    @Test
+    void namesFlux() {
+        // given
+
+        // when
+        var namesFlux = fluxAndMonoGeneratorService.namesFlux();
+        //then
+        StepVerifier.create(namesFlux)
+                // .expectNext("Gaurav", "G2", "g3")
+                //.expectNextCount(3)
+                .expectNext("Gaurav").expectNextCount(2).verifyComplete();
+    }
+
+    @Test
+    void namesFlux_map() {
+        {
+            // given
+
+            // when
+            var namesFlux = fluxAndMonoGeneratorService.namesFlux_map();
+            //then
+            StepVerifier.create(namesFlux)
+                    .expectNext("GAURAV", "G2", "G3")
+                    .verifyComplete();
+        }
+    }
+
+    @Test
+    void namesFlux_immutability() {
+        // given
+
+        // when
+        var namesFlux = fluxAndMonoGeneratorService.namesFlux_immutability();
+        //then
+        StepVerifier.create(namesFlux)
+                .expectNext("Gaurav", "G2", "g3")
+                .verifyComplete();
+    }
+
+    @Test
+    void testNamesFlux_map() {
+        //given
+        int stringLength = 3;
+        //when
+        var namesFlux = fluxAndMonoGeneratorService.namesFlux_map(stringLength);
+        //then
+        StepVerifier.create(namesFlux)
+                .expectNext("6-GAURAV")
+                .verifyComplete();
+    }
 }
