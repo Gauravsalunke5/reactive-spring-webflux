423d8de6813dd8904645a0122bb357581bff9c9d
Gaurav Salunke <gauravsalunke15@gmail.com>
2023-01-08 15:56:44 +0530

    web client, wireMock snd retry

--- movies-service/src/main/java/com/reactivespring/client/MoviesInfoRestClient.java
+++ movies-service/src/main/java/com/reactivespring/client/MoviesInfoRestClient.java
@@ -1,11 +1,20 @@
 package com.reactivespring.client;
 
 import com.reactivespring.domain.MovieInfo;
+import com.reactivespring.exception.MoviesInfoClientException;
+import com.reactivespring.exception.MoviesInfoServerException;
+import com.reactivespring.util.RetryUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+import reactor.util.retry.Retry;
 
+import java.time.Duration;
+
+@Slf4j
 @Component
 public class MoviesInfoRestClient {
 
@@ -19,11 +28,33 @@ public class MoviesInfoRestClient {
     }
 
     public Mono<MovieInfo> retrieveMovieInfo(String movieId) {
+
         var url = moviesInfoUrl.concat("/{id}");
+        /*var retrySpec = RetrySpec.fixedDelay(3, Duration.ofSeconds(1))
+                .filter((ex) -> ex instanceof MoviesInfoServerException)
+                .onRetryExhaustedThrow(((retryBackoffSpec, retrySignal) -> Exceptions.propagate(retrySignal.failure())));*/
+
         return webClient.get()
                 .uri(url, movieId)
                 .retrieve()
+                .onStatus(HttpStatus::is4xxClientError, (clientResponse -> {
+                    log.info("Status code : {}", clientResponse.statusCode().value());
+                    if (clientResponse.statusCode().equals(HttpStatus.NOT_FOUND)) {
+                        return Mono.error(new MoviesInfoClientException("There is no MovieInfo available for the passed in Id : " + movieId, clientResponse.statusCode().value()));
+                    }
+                    return clientResponse.bodyToMono(String.class)
+                            .flatMap(response -> Mono.error(new MoviesInfoClientException(response, clientResponse.statusCode().value())));
+                }))
+                .onStatus(HttpStatus::is5xxServerError, (clientResponse -> {
+                    log.info("Status code : {}", clientResponse.statusCode().value());
+                    return clientResponse.bodyToMono(String.class)
+                            .flatMap(response -> Mono.error(new MoviesInfoServerException(response)));
+                }))
                 .bodyToMono(MovieInfo.class)
+                //.retry(3)
+                //.retryWhen(Retry.fixedDelay(3, Duration.ofMillis(500)))
+                .retryWhen(RetryUtil.retrySpec())
                 .log();
+
     }
 }
